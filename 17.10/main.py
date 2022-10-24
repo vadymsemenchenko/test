27bf4630c29bb086d632416c184d5ade8cdd669d
Vadym Semenchenko <vadymsemenchenko@gmail.com>
2022-10-24 19:10:31 +0300

    changed main.py 17.10 and 20.10

---
+++ 17.10/main.py
@@ -0,0 +1,117 @@
+"""
+Hello
+world
+test
+"""
+'''
+
+'''
+
+# print("""22Hello
+# world
+# test""")
+# print('hello')  # функция выводит слово 'hello'
+# print('123')  # ...
+# print("123")
+# print(12)  # Integer - int
+# print(1.5 + 0.5)  # Float - float
+# print(0.1 + 0.1 + 0.1)  # 0.3
+
+
+# true or false
+# True - 1
+# False - 0  # boolean - bool
+
+# print(True + True + False)
+
+# print('Hello')
+# print('world')
+#
+# print('Hello', 'world', 1, 2, 3, 4, True, 1.5, sep='_')
+
+
+# name = 'Petr123'
+# user_email = 'admin@example.com'
+# x1 = 2
+# x2 = 3
+# y1 = -1
+# y2 = -2
+# y3 = 4
+# UserEmail = 'test@admin.com'
+#
+# print('Hello', name)  # asd
+# print('Email: ', user_email)
+# print('Email: ', UserEmail)
+
+
+# a = 'Hello', True, 1.2, 3.6, 5
+# b = ['Hello', True, 1.2, 3.6, 5]
+# c = {'Vasya', 'Petr', 'Ivan'}
+# d = {
+#     'Ivan': {
+#         'full_name': 'Ivan I.',
+#         'age': 25,
+#         'marks': [100, 99, 100]
+#     },
+#     'Petr': {
+#         'full_name': 'Petr I.',
+#         'age': 30,
+#         'marks': [98, 100, 99]
+#     },
+# }
+
+
+# print(a)
+# print(b)
+# b.append(4)
+# print(b[0:5:2])
+# print(c)
+#
+# print('Sasha' in c)
+# print(d['Petr']['marks'][1])
+# print(d['Ivan']['marks'][1])
+
+
+# a = 4  # 10
+#
+# print( hex( id(a) ) )
+#
+# a = a + 2  # 6
+#
+# print( hex( id(a) ) )
+#
+# b = 3 + 1  # 10
+# c = 2 + 2  # 10
+# d = 4  # 10
+# print('B = ', hex( id(b) ) )
+# print('C = ', hex( id(c) ) )
+# print('D = ', hex( id(d) ) )
+
+# a = 'Vasya'
+#
+# print(a)
+
+
+# a = 4  # 10
+# b = 5
+#
+# print(a is b)
+
+# print( hex( id(a) ) )
+#
+# a = a + 2  # 6
+#
+# print( hex( id(a) ) )
+#
+# print('B VAL')
+#
+# b = [1, 2]
+# print(b)
+#
+# print( hex( id(b) ) )
+#
+# b.extend([3, 4])
+#
+# print(b)
+# print( hex( id(b) ) )
+
---
+++ 20.10/main.py
@@ -0,0 +1,64 @@
+
+# print('Hello world')
+# name = input('Enter your name: ')
+# print(type(name))
+# print('Hello, ' + name)
+
+# eel
+# int
+# float
+# bool
+# str
+# a = int(input('A = '))
+# b = int(input('B = '))
+# print(type(a), type(b))
+# print(a + b)
+
+# print( '123'.isdigit() )
+# print( int('2.5') )
+# print( float('2.5') )
+# print( int( float('2.6') ) )  # print( 2 )
+# print( bool(print) )
+# print( str(print) )
+
+# print( 'Hello world'.split(' ') )
+
+
+# a = 123
+#
+# print('Your number is', a, '!')  # Your number is 123 !
+# print('Your number is ' + str(a) + '!')
+# print('Your %d number is %d!' % (a, 4))
+# print('Your {} number is {}!'.format(a, 4))
+# print(f'Your {2 + 2} number is {a}!')
+
+
+# print( 4 < 5 )
+# print( 4 > 5 )
+# print( 5 >= 5 )
+# print( 5 <= 10 )
+# print( 5 == '5' )
+# print( 5 != 10 )
+# print( 2 < 2.5 )
+
+# print( [2, 4, 3] >= [2, 3, 4] )
+
+# print( 4 + 2 )
+# print( 4 - 2 )
+# print( 4 * 2 )
+# print( 5 / 2 )
+# print( 4 ** 2 )
+# print( 5 // 2 )
+# print( 5 % 2 )
+#
+# a = 5
+#
+# a = a + 1  # a += 1
+# a = a - 1  # a -= 1
+# a = a ** 1  # a **= 1
+
+names = []
+
+if names:
+    print(names[0])
+
