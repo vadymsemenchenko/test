71018c775812eb55fecaa3be80bc32b6d625be0d
Vadym Semenchenko <vadymsemenchenko@gmail.com>
2022-12-08 17:19:19 +0200

    change ДЗ 11

--- ДЗ 11. Случайная строка/ДЗ.py
+++ ДЗ 11. Случайная строка/ДЗ.py
@@ -10,10 +10,19 @@
 
 
 
+import random, base64
+
+def get_random_string(n):
+    return base64.b64encode(random.randbytes(n//8*6+6)).replace(b'+', b'A').replace(b'/',b'B')[:n].decode()
+
+print(get_random_string(20))
+
+
+# исправленный
 # import random, base64
 #
 # def get_random_string(n):
-#     return base64.b64encode(random.randbytes(n//8*6+6)).replace(b'+', b'A').replace(b'/',b'B')[:n].decode()
+#     return base64.b64encode(random.randbytes(n), b'AB')[:n].decode()
 #
 # print(get_random_string(20))
 
@@ -38,6 +47,7 @@
 #     # Возвращаем сгенерированную строку
 #     return s
 #
-# Генерируем случайную строку длиной 10 символов
+#
+# # Генерируем случайную строку длиной 10 символов
 # s = get_random_string(10)
 # print(s)
