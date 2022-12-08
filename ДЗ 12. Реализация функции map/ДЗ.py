ec7640be7e9f0dbed8648c666bd522a78bec47b0
Vadym Semenchenko <vadymsemenchenko@gmail.com>
2022-12-08 17:19:53 +0200

    change ДЗ 12

--- ДЗ 12. Реализация функции map/ДЗ.py
+++ ДЗ 12. Реализация функции map/ДЗ.py
@@ -10,22 +10,22 @@
 #   pass
 
 
-# def custom_map(function, *iterables):
-#     out = []
-#     min_len = None
-#     for i in iterables:
-#         if min_len is None or len(i) < min_len:
-#             min_len = len(i)
-#     for i in range(min_len):
-#         args = []
-#         for j in iterables:
-#             args.append(j[i])
-#         out.append(function(*args))
-#     return out
-#
-# a = [1, 2, 3]
-# b = [2, 4, 0, 6]
-# c = [4, 6, 2, 8]
-# res = custom_map(lambda x, y, z: x + y + z, a, b, c)
-#
-# print(list(res))
+def custom_map(function, *iterables):
+    out = []
+    min_len = None
+    for i in iterables:
+        if min_len is None or len(i) < min_len:
+            min_len = len(i)
+    for i in range(min_len):
+        args = []
+        for j in iterables:
+            args.append(j[i])
+        out.append(function(*args))
+    return out
+
+a = [1, 2, 3]
+b = [2, 4, 0, 6]
+c = [4, 6, 2, 8]
+res = custom_map(lambda x, y, z: x + y + z, a, b, c)
+
+print(list(res))
