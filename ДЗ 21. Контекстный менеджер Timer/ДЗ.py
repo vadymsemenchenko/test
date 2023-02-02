dfef5b7a8fd6eef0b03957c522ef4eda01e35c90
Vadym Semenchenko <vadymsemenchenko@gmail.com>
2023-02-02 09:12:42 +0200

    create and change ДЗ 21

---
+++ ДЗ 21. Контекстный менеджер Timer/ДЗ.py
@@ -0,0 +1,45 @@
+# Написать класс Timer для измерения времени выполнения кода.
+# Прошедшее время должен возвращать в аттрибуте elapsed_time.
+# Класс должен применяться как контекстный менеджер.
+# У класса должен быть метод reset.
+# Если между использованием контекстного менеджера вызывался метод reset, прошедшее время должно считаться заново.
+# Если метод reset не вызывался, в elapsed_time должно быть сохранено время выполнения всех фрагментов кода обернутых контекстным менеджером.
+#
+# class Timer:
+#     pass
+# Пример использования:
+#
+# with Timer() as t:
+#     time.sleep(1)
+# print(t.elapsed_time)  # ~1 second
+# with t:
+#     time.sleep(2)
+# print(t.elapsed_time)  # ~3 seconds
+#
+# with Timer() as t2:
+#     time.sleep(1)
+# print(t2.elapsed_time)  # ~1 second
+# t2.reset()
+# with t2:
+#     time.sleep(2)
+# print(t2.elapsed_time)  # ~2 seconds
+
+
+class Timer:
+
+    def __init__(self):
+        self.elapsed_time = 0
+
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop_time = time.time()
+        self.elapsed_time =+ self.stop_time - self.start_time
+
+
+    def reset(selfself):
+        self.elapsed_time = 0
